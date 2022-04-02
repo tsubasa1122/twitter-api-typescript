99fed8baade799ca7b8d27931e0a7897a9af7d02
tsubasa <tsubasan1122@gmail.com>
2022-04-02 16:52:47 +0900

    dotenvを導入

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { Client } from "twitter-api-sdk";
+import UserRepository from "./repositories/twitter/userRepository";
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
 
-app.get("/", (_, res) => res.send("ok!!"));
+app.get("/", (_, res: Response) => res.send("ok!!"));
+
+// Twitter APIのClientを作成する
+const twitterClient = new Client(
+  process.env.TWITTER_BEARER_TOKEN || "test_token"
+);
+
+// ユーザー一覧を取得する;
+app.get("/twitter/users", (req: Request, res: Response) => {
+  const tweet = req.query.tweet;
+  const userRepository = new UserRepository(twitterClient);
+  res.send("ok");
+});
+
+// idに指定したユーザーのフォロワー一覧を取得する;
+app.get("/twitter/users/:id/follwers", (req: Request, res: Response) => {
+  res.send("ok");
+});
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
